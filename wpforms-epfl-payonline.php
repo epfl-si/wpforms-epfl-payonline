64963814435482b9ee9d7089bd633a3edc925a08
Nicolas Borboën <ponsfrilus@gmail.com>
2021-12-09 16:42:20 +0100

    [FIX] Set author to IDEV-FSD

--- wpforms-epfl-payonline.php
+++ wpforms-epfl-payonline.php
@@ -3,9 +3,10 @@
  * Plugin Name: WPForms EPFL Payonline
  * Plugin URI:  https://github.com/epfl-si/wpforms-epfl-payonline
  * Description: EPFL Payonline integration with WPForms.
- * Author:      Nicolas Borboën
- * Author URI:  https://go.epfl.ch/nbo
- * Version:     1.4.0
+ * Author:      EPFL IDEV-FSD
+ * Author URI:  https://go.epfl.ch/idev-fsd
+ * Contributor: Nicolas Borboën <nicolas.borboen@epfl.ch>
+ * Version:     1.5.0
  * Text Domain: wpforms-epfl-payonline
  * Domain Path: languages
  * License:     GPL-2.0+
